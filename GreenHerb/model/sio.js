07cef3a3881027eeec603bc17fe008b1468cafa6
kiwiyuki <kiwi.kyuui@gmail.com>
2014-07-18 01:39:36 +0900

    sioを更新

--- GreenHerb/model/sio.js
+++ GreenHerb/model/sio.js
@@ -4,5 +4,11 @@ module.exports = sio;
 
 function sio (server) {
 	var io = socket.listen(server);
-	io.set('transport',['websocket']);
+		// サーバー接続処理
+	io.socket.on('connection', function (socket) {
+		//first_msg
+		socket.emit('first_msg', function () {
+
+		});
+	});
 }
