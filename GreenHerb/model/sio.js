a0515cedb0c73041156fae7d7ae7a4fd63d9685f
ほつれ <cerceris326@gmail.com>
2014-07-18 19:11:55 +0900

    コメント追加

--- GreenHerb/model/sio.js
+++ GreenHerb/model/sio.js
@@ -6,19 +6,21 @@ module.exports = sio;
 
 function sio (server) {
 	var io = socket.listen(server);
+
 	// サーバー接続処理
 	io.sockets.on('connection', function (socket) {
+		// プレイヤーデータ初期化
 		var p = init(socket);
-		//初期化、データ送信
+		// データ送信
 		socket.json.emit('first message', p);
-		//全ユーザーデータ送信
+
 		//クライアントデータ受信
 		socket.json.on('client data', function (data) {
 			for(var i = 0; i < world_data.allPlayers.length; i++) {
 				if(data.id == world_data.allPlayers[i].id) {
 					world_data.allPlayers[i].x = data.x;
 					world_data.allPlayers[i].y = data.y;
-					console.log(world_data.allPlayers);
+					// console.log(world_data.allPlayers);
 					break;
 				}
 			}
@@ -27,23 +29,30 @@ function sio (server) {
 			// world_data.allPlayers[index].y = data.y;
 		});
 	});
+
+	// 全プレイヤーデータ送信（毎秒30回）
 	setInterval(function() {
 		io.sockets.json.emit('world data', world_data);
-	},33);
+	}, 33);
 }
 
+// プレイヤーデータ初期化
 function init(socket) {
 	var x = makePosition();
 	var y = makePosition();
 	var color = makeColor();
 	var p = new Player(socket.id, x, y, color);
 	world_data.allPlayers.push(p);
-// 	console.log(world_data.allPlayers);
+	// 	console.log(world_data.allPlayers);
 	return p;
 }
+
+// 座標生成
 function makePosition() {
 	return Math.round((Math.random() - 0.5) * 100);
 }
+
+// カラー生成
 function makeColor() {
-	return '#'+('00000'+(Math.random()*(1<<24)|0).toString(16)).slice(-6);
+	return '#'　+　('00000'　+　(Math.random()*(1<<24)　|　0).toString(16)).slice(-6);
 }
