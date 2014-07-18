e55c26c944ffa3b45dd371a89f777dcfaf726e61
kiwiyuki <kiwi.kyuui@gmail.com>
2014-07-18 15:21:17 +0900

    emit world data and on client data

--- GreenHerb/model/sio.js
+++ GreenHerb/model/sio.js
@@ -1,22 +1,35 @@
 var socket = require('socket.io');
 var Player = require('./game/player');
-var allPlayer = {};
+var world_data = {};
+world_data.allPlayers = [];
 module.exports = sio;
 
 function sio (server) {
 	var io = socket.listen(server);
 	// サーバー接続処理
 	io.sockets.on('connection', function (socket) {
-		var x = makePosition();
-		var y = makePosition();
-		var color = makeColor();
-		var p = new Player(socket.id, x, y, color);
-		//first_msg
+		var p = init();
+		//初期化、データ送信
 		socket.json.emit('first message', p);
+		//全ユーザーデータ送信
+		io.sockets.json.emit('world data', world_data);
+		//クライアントデータ受信
+		socket.json.on('client data', function(data) {
+			var index = world_data.allPlayers.indexOf(data.id);
+			world_data.allPlayers[index].x = data.x;
+			world_data.allPlayers[index].y = data.y;
+		});
 	});
-
 }
 
+function init() {
+	var x = makePosition();
+	var y = makePosition();
+	var color = makeColor();
+	var p = new Player(socket.id, x, y, color);
+	world_data.allPlayers.push(p);
+	return p;
+}
 function makePosition() {
 	return Math.round((Math.random() - 0.5) * 100);
 }
